77bb9e1c2558a5593aa42736ac90fb717489a70f
steel-name <biranramadhan637@gmail.com>
2025-06-07 10:40:42 +0700

    ubah commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const musicBtn = document.getElementById("music-btn");
 const bgm = document.getElementById("bgm");
 
 let level = 1;
-const maxLevel = 5;
+const maxLevel = 10;
 let score = 0;
 let flippedCards = [];
 let lock = false;
